f99ba79770d230486f95cea7a4f29a72d94f6806
Rouzbeh Moradian <94753643+Rouzbeh-mr@users.noreply.github.com>
2026-04-17 18:16:27 -0600

    Sort recommendations and add ranking index
    Sort recommendations by similarity score and add ranking.

--- recommender_app/app.py
+++ recommender_app/app.py
@@ -160,6 +160,10 @@ if st.button('Get Recommendations'):
         #return recommendations.sort_values(by='Similarity Score', ascending=False)
         recommendations = pd.merge(df_content[['title','genres','imdb_rating']], top_scores[['title','similarity_score']], on='title')
         recommendations.rename(columns={'title':'Movie Title', 'imdb_rating': 'IMDb Rating', 'similarity_score':'Similarity Score'}, inplace=True)
-        return recommendations.sort_values(by='Similarity Score', ascending=False).reset_index(drop=True)
+        recommendations = recommendations.sort_values(by='Similarity Score', ascending=False).reset_index(drop=True)
+        recommendations.index += 1
+        recommendations.index.name = "Rank"
+
+        return recommendations
 
     st.table(hybrid_recommender(new_userId))
